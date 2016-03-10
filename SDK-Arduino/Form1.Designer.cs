875c63845950ab8d6f5e9c37227cce283787c0b6
nowito <arrallatemillos@gmail.com>
2016-03-10 16:12:03 +0000

    cambios

--- SDK-Arduino/Form1.Designer.cs
+++ SDK-Arduino/Form1.Designer.cs
@@ -2,6 +2,8 @@
 {
     partial class Form1
     {
+
+
         /// <summary>
         /// Variable del diseñador necesaria.
         /// </summary>
@@ -28,21 +30,202 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(57, 43);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "115200"});
+            this.comboBox2.Location = new System.Drawing.Point(279, 43);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(57, 91);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(501, 48);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Send";
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(401, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(6, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(389, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.UseWaitCursor = true;
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.enterkey);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Location = new System.Drawing.Point(57, 166);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(501, 255);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Recieved";
+            // 
+            // textBox2
+            // 
+            this.textBox2.AcceptsReturn = true;
+            this.textBox2.Location = new System.Drawing.Point(6, 19);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(489, 230);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(591, 40);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 56);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Open";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(591, 110);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 56);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Close";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Ports";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(276, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Baud Rate";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(458, 40);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(455, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Status";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(749, 430);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
     }
 }
 
--- SDK-Arduino/Form1.cs
+++ SDK-Arduino/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
 
 namespace SDK_Arduino
 {
@@ -15,7 +16,103 @@ namespace SDK_Arduino
         public Form1()
         {
             InitializeComponent();
+            getAvailablePorts();
         }
 
+        void getAvailablePorts()
+        {
+            String[] ports = SerialPort.GetPortNames();
+            comboBox1.Items.AddRange(ports);
+        }
+
+        //public delegate void InvokeDelegate();
+
+        //public void text_Close()
+        //{
+        //    textBox2.Text = "Closeando\n";
+        //    serialPort1.Close();
+        //}
+        //public void text_Open()
+        //{
+        //    textBox2.Text = "**** Connected to " + comboBox1.Text + " @ " + comboBox2.Text + " baud ****\n";
+        //}
+
+        //public void text_EnterKey()
+        //{
+        //    textBox2.Text = ">>>>" + textBox1.Text + "\n";
+        //}
+        //public void text_DataReceived(string datain)
+        //{
+        //    textBox2.Text = datain + "\n";
+        //}
+
+        private void button3_Click(object sender, EventArgs e) // Open conection
+        {
+            try
+            {
+                if (comboBox1.Text == "" || comboBox2.Text == "")
+                    textBox2.Text = "Please select port settings\n";
+                else
+                {
+                    serialPort1.PortName = comboBox1.Text;
+                    serialPort1.BaudRate = Convert.ToInt32(comboBox2.Text);
+                    serialPort1.Open();
+                    if (serialPort1.IsOpen)
+                    {
+                        progressBar1.Value = 100;
+                        button1.Enabled = true;
+                        textBox1.Enabled = true;
+                        button3.Enabled = false;
+                        button4.Enabled = true;
+                        //textBox2.BeginInvoke(new InvokeDelegate(text_Open));
+                        textBox2.Invoke(new Action<string>(textBox2.AppendText), new object[] { "**** Connected to " + comboBox1.Text + " @ " + comboBox2.Text + " baud ****\n" });
+                    }
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                textBox2.Text = "Unauthorized Access\n";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e) // Close Connection
+        {
+            serialPort1.Close();
+            progressBar1.Value = 0;
+            if (serialPort1.IsOpen == false)
+            {
+                button1.Enabled = false;
+                textBox1.Enabled = false;
+                button3.Enabled = true;
+                button4.Enabled = false;
+                //textBox2.BeginInvoke(new InvokeDelegate(text_Close));
+                textBox2.Invoke(new Action<string>(textBox2.AppendText), new object[] { "**** Connection closed ****\n" });
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) // Send code with Send button
+        {
+            serialPort1.WriteLine(textBox1.Text);
+            textBox1.Text = "";
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting();
+            //textBox2.BeginInvoke(new InvokeDelegate(text_DataReceived));
+            textBox2.Invoke(new Action<string>(textBox2.AppendText), new object[] {indata});
+        }
+
+        private void enterkey(object sender, KeyEventArgs e) // Send code with Enter Key
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                serialPort1.WriteLine(textBox1.Text);
+                //textBox2.BeginInvoke(new InvokeDelegate(text_EnterKey));
+                textBox2.Invoke(new Action<string>(textBox2.AppendText), new object[] { ">>>>" + textBox1.Text + "\n" });
+                textBox1.Text = "";
+            }
+        }
     }
 }
